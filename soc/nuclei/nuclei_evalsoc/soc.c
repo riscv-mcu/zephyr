1b183b5a9593a8ac48cfd504ecd642de4296dc23
Huaqi Fang <578567190@qq.com>
2025-03-28 18:01:56 +0800

    fix(soc): enable ICache and DCache during early initialization

--- soc/nuclei/nuclei_evalsoc/soc.c
+++ soc/nuclei/nuclei_evalsoc/soc.c
@@ -7,10 +7,21 @@
 
 #include <zephyr/init.h>
 #include <zephyr/devicetree.h>
-
+#include <zephyr/arch/riscv/csr.h>
 
 void soc_early_init_hook(void)
 {
+	unsigned long mcfginfo = csr_read(0xFC2);
 
+	// Enable ICache and DCache when present
+	if (mcfginfo & (1<<9)) {
+		csr_set(0x7CA, (1<<0));
+	}
+	if (mcfginfo & (1<<10)) {
+		csr_set(0x7CA, (1<<16));
+	}
+	// Do fence
+	__asm__ volatile ("fence");
+	__asm__ volatile ("fence.i");
 
 }
