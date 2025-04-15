aebc558fdc6fc8aa8c0032a273c7031426ef4346
Huaqi Fang <578567190@qq.com>
2025-04-15 16:43:01 +0800

    fix(openocd): update board configuration and remove obsolete commands

--- boards/nuclei/fpga_eval/board.cmake
+++ boards/nuclei/fpga_eval/board.cmake
@@ -4,8 +4,12 @@
 
 # Need to use Nuclei OpenOCD which can be downloaded from https://www.nucleisys.com/download.php#tools
 
-board_runner_args(openocd "--cmd-pre-load=evalsoc-pre-load")
-board_runner_args(openocd "--cmd-load=evalsoc-load")
-board_runner_args(openocd "--cmd-post-verify=evalsoc-post-verify")
+set(OPENOCD_RAM_LOAD YES)
+
+board_runner_args(openocd "--config=${BOARD_DIR}/support/openocd.cfg")
+
+if(OPENOCD_RAM_LOAD)
+board_runner_args(openocd "--use-elf")
+endif()
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
